bbbc5dbe448c14dd13d31d00ecbd4829540c9918
xiayanzheng <xiayanzheng@outlook.com>
2018-09-27 22:14:43 +0800

    Add Kol Class for Learn

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.print("Nova!Spark!");
+        System.out.print("Nova!Spark!Yeah!Yeah!");
     }
 }
 
